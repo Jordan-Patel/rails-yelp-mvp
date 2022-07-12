9970c4869b5f402c399064658a4e2cdfaae4dac1
Jordan Patel <logicaljordan247@gmail.com>
2022-07-12 20:56:31 +0200

    app fully functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting old seed data'
+Restaurant.destroy_all
+
+puts 'Creating new restaurants...'
+
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
